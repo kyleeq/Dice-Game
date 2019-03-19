79fe04db1811f8b18d2faa4153cd4a2dce5be2dd
unknown <kylee.schreibs2@yahoo.com>
2019-03-19 15:42:18 -0500

    turn swithes to functions and add loop

--- unwind_take_a_hike.js
+++ unwind_take_a_hike.js
@@ -16,122 +16,142 @@ let num8 = 8;
 let num9 = 9;
 let num10 = 10;
 
-function initialPrompt(){
-	let question = prompt("Please enter your stress score.");
+function initialPrompt(stressPoints){
+	stressPoints = prompt("Please enter your stress score.");
+	stressPoints = Math.floor(parseInt(stressPoints));
+	console.log(stressPoints);
 
+	let diceArray = [9, 5, 4, 3, 8, 6];
+	
+	let functionsArray = [nineSided, fiveSided, fourSided, threeSided, eightSided, sixSided]
+	
+	for(let i = 0; i >= stressPoints; i++){
+
+	function nineSided(i){
+		functionsArray[0](rollDie(9))
+		if (i <= 3){
+			alert("Take three deep breaths.");
+			console.log(pointCalcDie());}
+		else if (i < 7){
+			alert("Take four deep breaths.");
+			console.log(pointCalcDie());}
+		else{
+			alert("Take five deep breaths.");
+			console.log(pointCalcDie());}
+	}
+}
+
+	// switch(rollDie(9)){
+	// 	case num1:
+	// 	case num2:
+	// 	case num3:
+						
+	// 		break;
+	// 	case num4:
+	// 	case num5:
+	// 	case num6:
+			
+	// 		break;
+	// 	case num7:
+	// 	case num8:
+	// 	case num9:
+	// 		alert
+	// 		break;
+	// }
+
+	// switch(rollDie(5)){
+	// 	case num1: 
+	// 		alert("Imagine sitting in a city park.");
+	// 		break;
+	// 	case num2:
+	// 		alert("Imagine yourself sitting in a park with a pond.");
+	// 		break;
+	// 	case num3:
+	// 		alert("");
+	// 		break;
+	// 	case num4:
+	// 		alert("Imagine yourself up north sitting on the dock of a small lake.");
+	// 		break;
+	// 	case num5:
+	// 		alert("Imagine yourself standing in a national park.");
+	// 		break;
+	// }
+
+
+	// switch(rollTwoDice(4)){
+	// 	case num2:
+	// 		alert("Early afternoon, 2 pm");
+	// 		break;
+	// 	case num3:
+	// 	case num4:
+	// 		alert("Noon");
+	// 		break;
+	// 	case num5:
+	// 	case num6:
+	// 		alert("Late morning, 9:30 am");
+	// 		break;
+	// 	case num7:
+	// 	case num8:
+	// 		alert("Early morning, 7 am");
+	// 		break;
+	// }
+
+	// switch(rollTwoDice(3)){
+	// 	case num2:
+	// 		alert("75 degrees with sporadic clouds.");
+	// 		break;
+	// 	case num3:
+	// 	case num4:
+	// 		alert("79 degrees and sunny");
+	// 		break;
+	// 	case num5:
+	// 	case num6:
+	// 		alert("82 degrees and lightly cloudy with a cool breeze.");
+	// 		break;
+	// }
+
+	// switch(rollDie(8)){
+	// 	case roll % 2 == 0:
+	// 			alert("Cup of coffee.");
+	// 			break;
+	// 		case roll % 2 != 0:
+	// 			alert("Cup of tea.");
+	// 			break;
+	// 	}
+
+	// 	switch(rollDie(6)){
+	// 		case num1:
+	// 			alert("Plane overhead");
+	// 		break;
+	// 	case num2:
+	// 		alert("Wood pecker");
+	// 		break;
+	// 	case num3:
+	// 		alert("Squirrels climbing trees");
+	// 		break;
+	// 	case num4:
+	// 		alert("Birds singing");
+	// 		break;
+	// 	case num5:
+	// 		alert("Rustling of trees and bushes");
+	// 		break;
+	// 	case num6:
+	// 		alert("Crickets");
+	// 		break;
+	// }
 
 
 }
 initialPrompt();
 
 
-
 function rollDie(max){
 	let roll = Math.floor(Math.random()* max + 1);
-	return roll;
-	
-function rollTwoDice(max){
-	let totalRoll = (Math.floor(Math.random()* max + 1)* 2);
-	return totalRoll;
+	console.log(roll);
 }
 
+function pointCalcDie(roll){
+	let currentScore = stressPoints - roll;
+	console.log("currentScore")
 }
 
-	switch(rollDie(9)){
-		case num1:
-		case num2:
-		case num3:
-			alert("Take three deep breaths.");
-			break;
-		case num4:
-		case num5:
-		case num6:
-			alert("Take four deep breaths.");
-			break;
-		case num7:
-		case num8:
-		case num9::
-			alert("Take five deep breaths.");
-			break;
-	}
-
-	switch(rollDie(5)){
-		case num1: 
-			alert("Imagine sitting in a city park.");
-			break;
-		case num2:
-			alert("Imagine yourself sitting in a park with a pond.");
-			break;
-		case num3:
-			alert("");
-			break;
-		case num4:
-			alert("Imagine yourself up north sitting on the dock of a small lake.");
-			break;
-		case num5:
-			alert("Imagine yourself standing in a national park.");
-			break;
-	}
-
-
-	switch(rollTwoDice(4)){
-		case num2:
-			alert("Early afternoon, 2 pm");
-			break;
-		case num3:
-		case num4:
-			alert("Noon");
-			break;
-		case num5:
-		case num6:
-			alert("Late morning, 9:30 am");
-			break;
-		case num7:
-		case num8:
-			alert("Early morning, 7 am");
-			break;
-	}
-
-	switch(rollTwoDice(3)){
-		case num2:
-			alert("75 degrees with sporadic clouds.");
-			break;
-		case num3:
-		case num4:
-			alert("79 degrees and sunny");
-			break;
-		case num5:
-		case num6:
-			alert("82 degrees and lightly cloudy with a cool breeze.");
-			break;
-	}
-
-	switch(rollDie(8)){
-		case totalRoll % 2 == 0:
-			alert("Cup of coffee.");
-			break;
-		case totalRoll % 2 != 0:
-			alert("Cup of tea.");
-			break;
-	}
-
-	switch(rollDie(6)){
-		case num1:
-			alert("Plane overhead");
-			break;
-		case num2:
-			alert("Wood pecker");
-			break;
-		case num3:
-			alert("Squirrels climbing trees");
-			break;
-		case num4:
-			alert("Birds singing");
-			break;
-		case num5:
-			alert("Rustling of trees and bushes");
-			break;
-		case num6:
-			alert("Crickets");
-			break;
-	}
